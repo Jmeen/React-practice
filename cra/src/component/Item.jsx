9b7c225d932e9eb5f7ae73ae9ffad1c66dfa2d15
Jaemin.Lim <jmeeen@gmail.com>
2021-09-23 13:54:06 +0900

    Props in Class Component

--- cra/src/component/Item.jsx
+++ cra/src/component/Item.jsx
@@ -9,13 +9,30 @@ function Item({ name, checked }) {
   // 구조 분해 할당, props에서 name필드 분해 후 할당
   //  조건부 랜더링
   // checked가 true면 "*" 넣기
+  // return (
+  //   <h3>
+  //     {checked ? <b>*</b> : null}
+  //     My item: {name}
+  //   </h3>
+  // );
+
+  // 조건부 렌더링 2. 논리 연산자 이용
   return (
     <h3>
-      {checked ? <b>*</b> : null}
+      {checked && <b>*</b>}
       My item: {name}
     </h3>
   );
 }
+//  props를 검사하기 (타입검사, 기본값 설정)
+Item.propTypes = {
+  name: PropTypes.string.isRequired, //  name prop: string, 필수 정보
+  checked: PropTypes.bool.isRequired, //  checked : bool, 필수 정보
+};
 
+Item.defaultProps = {
+  name: "Unnamed",
+  checked: true,
+};
 // 내보내기
 export default Item;
