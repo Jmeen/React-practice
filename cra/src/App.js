2a1726a9d35e68010f274238c2ddf9b8230613d4
Jaemin.Lim <jmeeen@gmail.com>
2021-09-23 10:47:51 +0900

    first component

--- cra/src/App.js
+++ cra/src/App.js
@@ -1,28 +1,43 @@
 import logo from './logo.svg';
 import './App.css';
+import Item from './component/Item'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          첫번째 리액트 앱입니다.
-        </p>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div classname="App">
+      <Item name="notebook"></Item>
+      <Item name="smartphone"></Item>
+      <Item name="tablet"></Item>
+      <Item name="monitor"></Item>
     </div>
-  );
+  )
 }
+
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           첫번째 리액트 앱입니다.
+//         </p>
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+
 
 export default App;
